4dc02d8c28ad9d226d91187bbe327b0f5b2c4600
azayyad48 <azayyad48@gmail.com>
2025-02-28 21:16:49 +0100

    code

--- main.tf
+++ main.tf
@@ -3,10 +3,17 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-04b4f1a9cf54c11d0" 
+  ami           = "ami-04b4f1a9cf54c11d0" # Ubuntu 22.04
   instance_type = "t2.micro"
 
   tags = {
     Name = "TODO-App-Server"
   }
 }
+
+resource "null_resource" "ansible_provision" {
+  provisioner "local-exec" {
+    command = "ansible-playbook -i inventory playbook.yml"
+  }
+}
+
