4e9c9d17c612b7ad96d96f3cd2570439d1daa02f
azayyad48 <azayyad48@gmail.com>
2025-02-28 12:12:45 +0100

    code

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-04b4f1a9cf54c11d0" 
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "TODO-App-Server"
+  }
+}
